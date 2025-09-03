d292447ca5975716375859815fdbaafc95f4f5c6
Anythingsoup01 <williamenglish1220@gmail.com>
2025-09-03 15:46:41 -0400

    Added Templates for Scene::OnComponentAdded()

--- CatEngine/src/CatEngine/Scene/Scene.cpp
+++ CatEngine/src/CatEngine/Scene/Scene.cpp
@@ -543,6 +543,17 @@ void Scene::OnRuntimeStop()
 	template<typename T>
 	void Scene::OnComponentAdded(Entity entity, T& component)
 	{
+        static_assert(false, "Component Not Supported!");
+	}
+    template<>
+	void Scene::OnComponentAdded<LayerComponent>(Entity entity, LayerComponent& component)
+	{
+
+	}
+    template<>
+	void Scene::OnComponentAdded<TagComponent>(Entity entity, TagComponent& component)
+	{
+
 	}
 	template<>
 	void Scene::OnComponentAdded<CameraComponent>(Entity entity, CameraComponent& component)
@@ -550,6 +561,48 @@ void Scene::OnRuntimeStop()
 		if (m_ViewportWidth > 0 && m_ViewportHeight > 0)
 			component.Camera.SetViewportSize(m_ViewportWidth, m_ViewportHeight);
 	}
+    template<>
+	void Scene::OnComponentAdded<TransformComponent>(Entity entity, TransformComponent& component)
+	{
+
+	}
+    template<>
+	void Scene::OnComponentAdded<CircleRendererComponent>(Entity entity, CircleRendererComponent& component)
+	{
+
+	}
+
+    template<>
+	void Scene::OnComponentAdded<SpriteRendererComponent>(Entity entity, SpriteRendererComponent& component)
+	{
+
+	}
+    template<>
+	void Scene::OnComponentAdded<Rigidbody2DComponent>(Entity entity, Rigidbody2DComponent& component)
+	{
+
+	}
+
+    template<>
+	void Scene::OnComponentAdded<NameComponent>(Entity entity, NameComponent& component)
+	{
+
+	}
+    template<>
+	void Scene::OnComponentAdded<BoxCollider2DComponent>(Entity entity, BoxCollider2DComponent& component)
+	{
+
+	}
+    template<>
+	void Scene::OnComponentAdded<CircleCollider2DComponent>(Entity entity, CircleCollider2DComponent& component)
+	{
+
+	}
+    template<>
+    void Scene::OnComponentAdded<ScriptComponent>(Entity entity, ScriptComponent& component)
+    {
+
+    }
 
 
 }
